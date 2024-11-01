e119ebeb637bcb786579de6d01ad8b1041951aef
Ajknight121 <aknig2@uic.edu>
2024-10-31 20:49:10 -0500

    Add react router, desktop abstract page, home layout
    The abstract page has an example gform and the "side header" is not mobile responsive

---
+++ src/Abstract.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import convergelogo from "./images/convergehealth.png";
+
+export default function Abstract() {
+  return (
+    <div className="abstract-page">
+      <div className="side-space">
+        <div className="header side">
+          <img src={convergelogo} />
+        </div>
+      </div>
+      
+      <div className="form">
+        <div>SUBMIT ABSTRACT</div>
+        <div style={{ margin: "auto" }}>
+          <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSdSF4H7L3YvSNH19S8Q6Fd7zAFYUr11_VV-dTmqv8ysCd9Yww/viewform?embedded=true"
+            width="640"
+            height="1788"
+            frameBorder="0"
+            marginHeight="0"
+            marginWidth="auto"
+          >
+            Loading…
+          </iframe>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import convergelogo from "./images/convergehealth.png";
+import { Link } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="header">
+        <img src={convergelogo} height={"90%"} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="content">
+        <div className="countdown">COUNTDOWN 00:00:00</div>
+        <div className="intro">
+          MAIN BODY TEXT GOES HERE, INTRODUCTION INTO THE CONFRENCE
+        </div>
+        <div className="nav">
+          <Link to={"/abstract"}>
+            <button className="submit-abstract">
+              <div className="overlay">
+                <div className="abstract-button">SUBMIT ABSTRACT</div>
+              </div>
+            </button>
+          </Link>
+          <Link to={"/about"}>
+            <button className="learn-more">
+              <div className="overlay">
+                <div className="learn-button">LEARN MORE</div>
+              </div>
+            </button>
+          </Link>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
