1fcc94e18639634c9d06366a1df311ee7d9668ef
Ajknight121 <aknig2@uic.edu>
2024-11-25 14:10:39 -0600

    Home layout sandwich

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -5,9 +5,9 @@ import convergelogo from "../images/convergehealth.png";
 import { AnimationContext } from "../AnimateContext";
 
 export default function Home() {
-  const {isAnimating, setIsAnimating } = useContext(AnimationContext)
+  const { isAnimating, setIsAnimating } = useContext(AnimationContext);
   // Goal date (you can change this as needed)
-  const goalDate = new Date(2025, 3-1, 14, 12,0); // year, month, day, hours, minutes, seconds
+  const goalDate = new Date(2025, 3 - 1, 14, 12, 0); // year, month, day, hours, minutes, seconds
   console.log(goalDate.toLocaleDateString());
   console.log(goalDate.toTimeString());
 
@@ -16,7 +16,7 @@ export default function Home() {
     setTimeout(() => {
       document.querySelector(".header").classList.remove("full");
       document.querySelector(".content").classList.remove("full");
-      setIsAnimating(false)
+      setIsAnimating(false);
     }, 1500);
 
     // Cleanup function to remove the event listener when the component unmounts
@@ -27,7 +27,6 @@ export default function Home() {
 
   return (
     <div className="home">
-
       <div className={`header ${isAnimating && "full"}`}>
         <div className="logo">
           <Link to={"/"}>
@@ -40,20 +39,24 @@ export default function Home() {
           <Countdown goalDate={goalDate} />
         </div>
 
+        <div className="nav">
+          <a
+            href="https://submit.convergehealth.cc"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="mobile-button">SUBMIT ABSTRACT</div>
+          </a>
+          <Link to={"/about"}>
+            <div className="mobile-button">MORE INFORMATION</div>
+          </Link>
+        </div>
         <div className="intro">
           CONVERGE HEALTH is set for March 14th, 2025! Join us and witness how
           cutting-edge research in Biology, Computer Science, and Engineering
           converge in healthcare.
         </div>
-        <div className="nav">
-        <a href="https://submit.convergehealth.cc" target="_blank" rel="noopener noreferrer">
-          <div className="mobile-button">SUBMIT ABSTRACT</div>
-        </a>
-        <Link to={"/about"}>
-          <div className="mobile-button">MORE INFORMATION</div>
-        </Link>
-        </div>
       </div>
     </div>
-  )
+  );
 }
